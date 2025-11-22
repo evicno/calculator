3ca6a01b0f75fcedaeb0a0ccfe2ec26118506ce8
evicno <229501549+evicno@users.noreply.github.com>
2025-11-22 14:05:48 +0100

    Add top display for current operation display

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@
 let num1 = "0";
 let operator = "";
 let num2 = "0";
+let operationOver = false;
 
 // Create arrays with all operators and numbers buttons
 //const operators = Array.from(document.querySelectorAll(".operator"));
@@ -12,39 +13,60 @@ let num2 = "0";
 // Case the button clicked is an operator
 function clickOperator(op) {
     let result = 0;
-    if (op == "equal") {
-        switch (operator) {
-            case "add":
-                result = (Number(num1) + Number(num2));
-                break;
-            case "sub":
-                result = (Number(num1) - Number(num2));
-                break;
-            case "mult":
-                result = (Number(num1) * Number(num2));
-                break;
-            case "div":
-                result = (Number(num1) / Number(num2));
-                break;
-        }
+    switch (op) {
+        case "add":
+            topDisplay.textContent += "+";
+            operator = op;
+            break;
+        case "sub":
+            topDisplay.textContent += "-";
+            operator = op;
+            break;
+        case "mult":
+            topDisplay.textContent += "x";
+            operator = op;
+            break;
+        case "div":
+            topDisplay.textContent += "/";
+            operator = op;
+            break;
+        case "equal":
+            switch (operator) {
+                case "add":
+                    result = (Number(num1) + Number(num2));
+                    break;
+                case "sub":
+                    result = (Number(num1) - Number(num2));
+                    break;
+                case "mult":
+                    result = (Number(num1) * Number(num2));
+                    break;
+                case "div":
+                    result = (Number(num1) / Number(num2));
+                    break;
+            };
         bottomDisplay.textContent = result;
         num1 = "0";
         num2 = "0";
         operator = "";
+        operationOver = true;
     }
-    else operator = op;
 
 }
 
 // Case the button clicked is a number (uses newOperation to decide if the number
 // should be added to num1 or num2)
 function clickNumber(num) {
-    if (operator == "") {
-        num1 = addNumber(num, num1);
+    if (operator == "") { 
+        if (operationOver && num1 == "0") { //Clears the top display for new operation
+            topDisplay.textContent = ""; 
+        }
+        num1 = addNumber(num, num1); 
     }
     else {
         num2 = addNumber(num, num2);
     }
+    topDisplay.textContent += num;
 }
 
 // Add the digit clicked to the number: replaces number by digit if number = 0,
@@ -60,6 +82,7 @@ function addNumber(digit, number) {
 }
 
 const bottomDisplay = document.querySelector("#bottom");
+const topDisplay = document.querySelector("#top");
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
