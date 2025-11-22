ac91b85c49da49fbaf9c92ce9b6d90f130baf381
evicno <229501549+evicno@users.noreply.github.com>
2025-11-22 13:37:05 +0100

    Add clickNumber(num), calculator works for 4 op Handles multiple digits numbers (only integers)

--- javascript.js
+++ javascript.js
@@ -1,43 +1,71 @@
-// Variables to save data of the operation
-let num1 = 0;
+// Variables to save data of the operation.
+// We use strings to add new numbers to num1 and num2 (only convert it to Number
+// in clickOperator)
+let num1 = "0";
 let operator = "";
-let num2 = 0;
+let num2 = "0";
 
 // Create arrays with all operators and numbers buttons
 //const operators = Array.from(document.querySelectorAll(".operator"));
 //const numbers = Array.from(document.querySelectorAll(".number"));
 
+// Case the button clicked is an operator
 function clickOperator(op) {
     if (op == "equal") {
-        console.log("equal");
         switch (operator) {
             case "add":
-                console.log(num1 + num2);
+                console.log(Number(num1) + Number(num2));
+
                 break;
             case "sub":
-                console.log(num1 - num2);
+                console.log(Number(num1) - Number(num2));
                 break;
             case "mult":
-                console.log(num1 * num2);
+                console.log(Number(num1) * Number(num2));
                 break;
             case "div":
-                console.log(num1 / num2);
+                console.log(Number(num1) / Number(num2));
                 break;
         }
+        num1 = "0";
+        num2 = "0";
+        operator = "";
     }
     else operator = op;
 
+}
+
+// Case the button clicked is a number (uses newOperation to decide if the number
+// should be added to num1 or num2)
+function clickNumber(num) {
+    if (operator == "") {
+        num1 = addNumber(num, num1);
+    }
+    else {
+        num2 = addNumber(num, num2);
+    }
+}
+
+// Add the digit clicked to the number: replaces number by digit if number = 0,
+// else concatenates the digit to number.
+function addNumber(digit, number) {
+    if (number == "0") {
+        return digit;
+    }
+    else {
+        number += digit;
+        return number;
+    }
 }
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         if (button.className == "operator") {
-            console.log(button.id);
             clickOperator(button.id);
         }
         else if (button.className == "number") {
-            num1 = Number(button.id);
+            clickNumber(button.id);
         }
     })
 })
