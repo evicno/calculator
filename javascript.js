1fbf98bdd794e3abcb67f5e1f79fb4589d987992
evicno <229501549+evicno@users.noreply.github.com>
2025-11-23 16:13:15 +0100

    Add clearAllVariables function and multiple fixes Simplify most functions, fix bugs with multiple operations. Only works with console so far.

--- javascript.js
+++ javascript.js
@@ -4,8 +4,7 @@
 let num1 = "";
 let operator = "";
 let num2 = "";
-let operationOver = false;
-let result = 0;
+let result = ""; // result only changes after = being entered
 let lastButton = [];
 
 // Create arrays with all operators and numbers buttons
@@ -29,113 +28,111 @@ function operate(num1, num2, operator) {
     }
     return result;
 }
+// Process the = button being clicked.
+// Ignores it if 2 numbers and one operator have not been entered.
+function clickEqual () {
+    point.disabled = false;
+    if (num1 == "" || operator == "" || num2 == "") {
+            return;
+        }
+    else { // operate and put it in result, clears other variables
+        result = operate(num1, num2, operator);
+        num1 = "";
+        num2 = "";
+        operator = "";
+        lastButton = [];
+
+    }
+    console.log(num1, num2, operator, result);
+}
 
 // Case the button clicked is an operator
 function clickOperator(op) {
     point.disabled = false;
-    if (op == "equal"){
-        if (num1 == "" || operator == "" || num2 == "") {
-            return;
-        }
-        else {
-            result = operate(num1, num2, operator);
-            topDisplay.textContent = bottomDisplay.textContent;
-            bottomDisplay.textContent = roundResultForDisplay(result);
-            num1 = "";
-            num2 = "";
-            operator = "";
-            operationOver = true;
-        }
+    let last = lastButton[lastButton.length - 1];
+    // If op is a second consecutive operator entered, erase the first one and
+    // start over with the new one.
+    if (last == "operator") {
+        operator = op;
     }
-    else {
-        console.log(num1, num2, operator, result);
-        if (result != 0) { // an operation just occured, we want to use the result as num1
-            num1 = result;
-            operator = op;
-            console.log(num1, num2, operator, result);
-            addOperatorDisplay(op, "bottom");
+    //        topDisplay.textContent = bottomDisplay.textContent;
+     //       bottomDisplay.textContent = roundResultForDisplay(result);
+    if (result != 0) { // an operation just finished, we want to use the result as num1
+        num1 = result;
+        result = "";
+        operator = op; 
+        //   topDisplay.textContent = bottomDisplay.textContent;
+         //   addOperatorDisplay(op, "bottom");
+            //result = operate(num1, num2, operator);
+
+           // bottomDisplay.textContent = roundResultForDisplay(result);
+           // addOperatorDisplay(op, "bottom");
         }
-        if (operator != "") { // add a new operation (without =)
-            console.log(num1, num2, operator, result);
-            let last = lastButton[lastButton.length - 1];
-            if (last == "operator") {
-                console.log(num1, num2, operator, result);
-                clickClear("c");
-                clickOperator(op);
-                return;
-            }
-            console.log(num1, num2, operator, result);
-            result = operate(num1, num2, operator);
-            topDisplay.textContent = bottomDisplay.textContent;
-            bottomDisplay.textContent = roundResultForDisplay(result);
-            num1 = result;
-            num2 = "";
-            operator = op;
-            addOperatorDisplay(op, "bottom");
-        }
-        else { //
-            addOperatorDisplay(op, "bottom");
+    else if (operator != "") { // add a new operation (without =)
+            //topDisplay.textContent = bottomDisplay.textContent;
+            //bottomDisplay.textContent = roundResultForDisplay(result);
+        num1 = operate(num1, num2, operator);
+        num2 = "";
+        operator = op;
+            //addOperatorDisplay(op, "bottom");
+            //console.log(num1, num2, operator, result);
+    }
+    else { // only num1 has been entered yet
+            //addOperatorDisplay(op, "bottom");
             operator = op;
         }
-        lastButton.push("operator");
-    }   
-}
+    lastButton.push("operator");
+    console.log(num1, num2, operator, result);
+}   
+
 
 // Case the button clicked is a number (uses newOperation to decide if the number
 // should be added to num1 or num2)
 function clickNumber(num) {
     if (num == "point") { //allow to handle point like a number
-        num = ".";
+        num = "0.";
         point.disabled = true;
     }
     if (operator == "") { //first number of operation
-        result = 0;
-        topDisplay.textContent = "";
+        result = "";
+        //topDisplay.textContent = "";
         if (num1 == "" && num == ".") { // add a 0 before the .
-            num1 = "0.";
-            bottomDisplay.textContent = "0.";
+            //num1 = "0.";
+            //bottomDisplay.textContent = "0.";
         }
         else {
         num1 = addNumber(num, num1);
-        bottomDisplay.textContent = num1; 
+        // bottomDisplay.textContent = num1; 
         }
         lastButton.push("num1");
     }
     else { // num2 is being entered
         // case of div by 0
         if (num2 == "" && num == "0") {
-            topDisplay.textContent = "";
-            bottomDisplay.textContent = "Error (div by 0)";
-            num1 = "";
-            num2 = "";
-            operator = "";
-            result = 0;
-            lastButton = [];
-            operationOver = false;
+            //topDisplay.textContent = "";
+            //bottomDisplay.textContent = "Error (div by 0)";
+            clearAllVariables();
         }
         else {
             if (num2 == "" && num == ".") {
-                console.log(1);
-                num2 = "0."
+                //num2 = "0."
                 bottomDisplay.textContent += "0.";
             }
             else {
                 num2 = addNumber(num, num2);
-                bottomDisplay.textContent += num;
+                //bottomDisplay.textContent += num;
             }
         lastButton.push("num2");
         }
     }
+    console.log(num1, num2, operator, result);
 }
 
 // Add the digit clicked to the number: replaces number by digit if number = 0,
 // else concatenates the digit to number.
 function addNumber(digit, number) {
     if (number == "") {
-        if (digit == ".") {
-            return "0."
-        }
-        else return digit;
+        return digit; // works for . and number
     }
     else {
         number += digit;
@@ -143,20 +140,21 @@ function addNumber(digit, number) {
     }
 }
 
-function clickClear(id) {
-    if (id == "ac") {
+function clearAllVariables () {
         num1 = "";
         num2 = "";
         operator = "";
         result = "";
-        lastButton = "";
-        operationOver = false;
         lastButton = [];
-        topDisplay.textContent = "";
-        bottomDisplay.textContent = 0;
+}
+function clickClear(id) {
+    if (id == "ac") {
+        clearAllVariables();
+        //topDisplay.textContent = "";
+        //bottomDisplay.textContent = 0;
     }
     if (id == "c") {
-        bottomDisplay.textContent = bottomDisplay.textContent.slice(0, bottomDisplay.textContent.length - 1);
+        //bottomDisplay.textContent = bottomDisplay.textContent.slice(0, bottomDisplay.textContent.length - 1);
         let last = lastButton.pop();
         switch (last) {
             case ("num1"):
@@ -172,7 +170,7 @@ function clickClear(id) {
     }
 }
 
-function addOperatorDisplay(op, display) {
+function addElementToDisplay(element, display) {
     if (display == "top"){
         switch(op) {
             case "add":
@@ -231,7 +229,12 @@ const point = document.querySelector("#point");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         if (button.className == "operator") {
-            clickOperator(button.id);
+            if (button.id == "equal") {
+                clickEqual();
+            }
+            else { // case of add, sub, mult and div
+                clickOperator(button.id);
+            }
         }
         else if (button.className == "number") {
             clickNumber(button.id);
